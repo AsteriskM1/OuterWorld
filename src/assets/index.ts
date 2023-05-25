07094487ea7b1a38d76b3a7909428cbf321773d5
Justcallme-H <akritmhj7@gmail.com>
2023-05-25 12:13:03 +0545

    added assets

---
+++ src/assets/index.ts
@@ -0,0 +1,65 @@
+import crewBgDesktop from './crew/background-crew-desktop.jpg';
+import crewBgTablet from './crew/background-crew-tablet.jpg';
+import crewBgMobile from './crew/background-crew-mobile.jpg';
+import anousheh from './crew/image-anousheh-ansari.webp';
+import douglas from './crew/image-douglas-hurley.webp';
+import mark from './crew/image-mark-shuttleworth.webp';
+import victor from './crew/image-victor-glover.webp';
+
+import destBgDesktop from './destination/background-destination-desktop.jpg';
+import destBgTablet from './destination/background-destination-tablet.jpg';
+import destBgMobile from './destination/background-destination-mobile.jpg';
+import europa from './destination/image-europa.webp';
+import mars from './destination/image-mars.webp';
+import moon from './destination/image-moon.webp';
+import titan from './destination/image-titan.webp';
+
+import homeBgDesktop from './home/background-home-desktop.jpg';
+import homeBgTablet from './home/background-home-tablet.jpg';
+import homeBgMobile from './home/background-home-mobile.jpg';
+
+import close from './shared/icon-close.svg';
+import menu from './shared/icon-hamburger.svg';
+import logo from './shared/logo.svg';
+
+import techBgDesktop from './technology/background-technology-desktop.jpg';
+import techBgTablet from './technology/background-technology-tablet.jpg';
+import techBgMobile from './technology/background-technology-mobile.jpg';
+import launchVehSm from './technology/image-launch-vehicle-landscape.jpg';
+import launchV from './technology/image-launch-vehicle-portrait.jpg';
+import spaceCapSm from './technology/image-space-capsule-landscape.jpg';
+import spaceCap from './technology/image-space-capsule-portrait.jpg';
+import spaceportSm from './technology/image-spaceport-landscape.jpg';
+import spaceport from './technology/image-spaceport-portrait.jpg';
+
+export {
+    crewBgDesktop,
+    crewBgTablet,
+    crewBgMobile,
+    anousheh,
+    douglas,
+    mark,
+    victor,
+    destBgDesktop,
+    destBgTablet,
+    destBgMobile,
+    europa,
+    mars,
+    moon,
+    titan,
+    homeBgDesktop,
+    homeBgTablet,
+    homeBgMobile,
+    close,
+    menu,
+    logo,
+    techBgDesktop,
+    techBgTablet,
+    techBgMobile,
+    launchVehSm,
+    launchV,
+    spaceCapSm,
+    spaceCap,
+    spaceportSm,
+    spaceport,
+}
